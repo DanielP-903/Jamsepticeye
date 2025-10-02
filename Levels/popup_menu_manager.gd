6be84fd8c7e9ea33715563b239054e5cd949d7f6
Dan <danpaulgamedev@gmail.com>
2025-10-02 12:58:18 +0100

    Set main menu scene

---
+++ Levels/popup_menu_manager.gd
@@ -0,0 +1,14 @@
+extends MarginContainer
+
+@export var menu_screen: VBoxContainer
+@export var open_menu_screen: VBoxContainer
+
+func toggle_visibility(object):
+	if !object:
+		return
+		
+	object.visible = !object.visible
+
+func _on_toggle_menu_button_pressed():
+	toggle_visibility(menu_screen)
+	toggle_visibility(open_menu_screen)
